679196e4c68cf5f4808b4bace257b4ac83cd4749
Junyoung Yang <tom9744@@gmail.com>
2025-04-06 22:20:10 +0900

    클라이언트 목록 관리 서비스 분리

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,10 +1,11 @@
+using ChatServer.Services;
 using System.Net.WebSockets;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var clients = new List<WebSocket>();
+var chatService = new ChatService();
 
 app.UseWebSockets();
 
@@ -13,8 +14,7 @@ app.Map("/chat", async context =>
     if (context.WebSockets.IsWebSocketRequest)
     {
         var client = await context.WebSockets.AcceptWebSocketAsync();
-        clients.Add(client);
-        Console.WriteLine("클라이언트 연결.");
+        chatService.AddClient(client);
 
         var buffer = new byte[1024 * 4];
 
@@ -25,23 +25,14 @@ app.Map("/chat", async context =>
             if (receivedResult.MessageType == WebSocketMessageType.Close)
             {
                 await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "연결 해제", CancellationToken.None);
-                Console.WriteLine("클라이언트 연결 해제.");
-                clients.Remove(client);
+                chatService.RemoveClient(client);
                 break;
             }
 
             var message = Encoding.UTF8.GetString(buffer, 0, receivedResult.Count);
             Console.WriteLine($"받은 메시지: {message}");
 
-
-            // Broadcast the message to all connected clients expluding the sender
-            foreach (var otherClient in clients)
-            {
-                if (otherClient != client && otherClient.State == WebSocketState.Open)
-                {
-                    await otherClient.SendAsync(new ArraySegment<byte>(buffer, 0, receivedResult.Count), receivedResult.MessageType, receivedResult.EndOfMessage, CancellationToken.None);
-                }
-            }
+            await chatService.BroadcastMessageAsync(message, client);
         }
     }
     else
---
+++ ChatServer/Services/ChatService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace ChatServer.Services
+{
+    public class ChatService
+    {
+        private readonly List<WebSocket> _clients = new();
+
+        public void AddClient(WebSocket socket)
+        {
+            _clients.Add(socket);
+            Console.WriteLine("클라이언트 연결.");
+        }
+        public void RemoveClient(WebSocket socket)
+        {
+            _clients.Remove(socket);
+            Console.WriteLine("클라이언트 연결 해제.");
+        }
+
+        public async Task BroadcastMessageAsync(string message, WebSocket sender)
+        {
+            var buffer = Encoding.UTF8.GetBytes(message);
+
+            foreach (var otherClient in _clients)
+            {
+                if (otherClient != sender && otherClient.State == WebSocketState.Open)
+                {
+                    await otherClient.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
+        }
+    }
+}
