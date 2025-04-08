5ec4dd2bab5c1425407f24a38fa8f5c15ba1b8c2
Junyoung Yang <tom9744@@gmail.com>
2025-04-09 00:12:03 +0900

    동시성 문제 해결을 위해 ConcurrentDictionary 도입

--- ChatServer/Services/ChatService.cs
+++ ChatServer/Services/ChatService.cs
@@ -1,5 +1,6 @@
 ﻿using ChatServer.Models;
 using ChatServer.Utils;
+using System.Collections.Concurrent;
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Encodings.Web;
@@ -9,59 +10,51 @@ namespace ChatServer.Services
 {
     public class ChatService
     {
-        private readonly Dictionary<WebSocket, string> _clients = new();
+        private readonly ConcurrentDictionary<WebSocket, string> _clients = new();
 
         public async Task AddClientAsync(WebSocket client)
         {
-            if (_clients.ContainsKey(client))
-            {
-                return;
-            }
-
             var nickname = NicknameGenerator.Generate();
 
-            _clients[client] = nickname;
-            Console.WriteLine($"클라이언트 연결됨: {nickname}");
-
-            var welcomeMessage = new ChatMessage
+            if (_clients.TryAdd(client, nickname))
             {
-                Nickname = nickname,
-                Type = ChatMessageType.Welcome,
-                Text = $"환영합니다, {nickname}님!"
-            };
+                Console.WriteLine($"클라이언트 연결됨: {nickname}");
 
-            await SendMessageAsync(client, welcomeMessage);
+                var welcomeMessage = new ChatMessage
+                {
+                    Nickname = nickname,
+                    Type = ChatMessageType.Welcome,
+                    Text = $"환영합니다, {nickname}님!"
+                };
 
-            var joinMessage = new ChatMessage
-            {
-                Nickname = nickname,
-                Type = ChatMessageType.Join,
-                Text = $"{nickname}님이 입장하셨습니다."
-            };
+                await SendMessageAsync(client, welcomeMessage);
 
-            await BroadcastMessageAsync(client, joinMessage);
+                var joinMessage = new ChatMessage
+                {
+                    Nickname = nickname,
+                    Type = ChatMessageType.Join,
+                    Text = $"{nickname}님이 입장하셨습니다."
+                };
+
+                await BroadcastMessageAsync(client, joinMessage);
+            }
         }
 
         public async Task RemoveClientAsync(WebSocket client)
         {
-            _clients.TryGetValue(client, out var nickname);
-
-            if (nickname == null)
+            if (_clients.TryRemove(client, out var nickname))
             {
-                return;
+                Console.WriteLine($"클라이언트 연결 해제: {nickname}");
+
+                var message = new ChatMessage
+                {
+                    Nickname = nickname,
+                    Type = ChatMessageType.Leave,
+                    Text = $"{nickname}님이 나갔습니다."
+                };
+
+                await BroadcastMessageAsync(client, message);
             }
-
-            _clients.Remove(client);
-            Console.WriteLine($"클라이언트 연결 해제: {nickname}");
-
-            var message = new ChatMessage
-            {
-                Nickname = nickname,
-                Type = ChatMessageType.Leave,
-                Text = $"{nickname}님이 나갔습니다."
-            };
-
-            await BroadcastMessageAsync(client, message);
         }
 
         public async Task HandleMessageAsync(WebSocket client, string json)
